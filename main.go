9329b6f260df84897be93f9472c8e417ef051729
Guus van Weelden <guus@weelden.de>
2017-11-08 17:17:36 +0100

    add exposing metrics

--- main.go
+++ main.go
@@ -5,11 +5,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-	address = flag.String("address", ":8080", "The address to listen on")
+	once           = &sync.Once{}
+	prometheusAddr = flag.String("prometheus-address", ":8085", "The Address on which the prometheus handler should be exposed")
+	prometheusPath = flag.String("prometheus-path", "/metrics", "The path on the host, on which the handler is available")
+	address        = flag.String("address", ":8080", "The address to listen on")
 )
+
+// ServeForever the prometheus metrics endpoint
+func ServeForever(addr, path string) {
+	once.Do(func() {
+		log.Printf("Prometheus metrics exposed on: %s%s", addr, path)
+
+		http.Handle(path, promhttp.Handler())
+		log.Fatal(http.ListenAndServe(addr, nil))
+	})
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
@@ -17,6 +33,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	flag.Parse()
+
+	go ServeForever(*prometheusAddr, *prometheusPath)
 
 	log.Printf("Listening on %s", *address)
 	http.HandleFunc("/", handler)
